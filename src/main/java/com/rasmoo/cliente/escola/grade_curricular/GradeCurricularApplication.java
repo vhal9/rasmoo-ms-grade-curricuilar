5062e4791f4da94214e3451dfd5268175e888c25
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-09-13 22:00:51 -0300

    Create project

---
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/GradeCurricularApplication.java
@@ -0,0 +1,13 @@
+package com.rasmoo.cliente.escola.grade_curricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCurricularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasmoo/cliente/escola/grade_curricular/GradeCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasmoo.cliente.escola.grade_curricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
