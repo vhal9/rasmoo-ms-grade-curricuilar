f7143bf80328aeae590e2cb0994712b2ea71a40e
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-09-26 20:42:03 -0300

    corrige http status da exceção de materia not found

--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/exceptions/MateriaNotFoundException.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/exceptions/MateriaNotFoundException.java
@@ -3,7 +3,7 @@ package com.rasmoo.cliente.escola.grade_curricular.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class MateriaNotFoundException extends Exception {
 
     public MateriaNotFoundException(Long id) {
