27422274fc424c66bc962507b5b3d7be8c2fad9d
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-10-14 20:00:20 -0300

    adiciona verificação de id para criação de materia

--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/controllers/MateriaController.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/controllers/MateriaController.java
@@ -1,16 +1,12 @@
 package com.rasmoo.cliente.escola.grade_curricular.controllers;
 
 import com.rasmoo.cliente.escola.grade_curricular.exceptions.MateriaNotFoundException;
+import com.rasmoo.cliente.escola.grade_curricular.exceptions.SendIdException;
 import com.rasmoo.cliente.escola.grade_curricular.models.dto.MateriaDTO;
 import com.rasmoo.cliente.escola.grade_curricular.models.dto.MessageResponseDTO;
 import com.rasmoo.cliente.escola.grade_curricular.models.dto.ResponseDTO;
-import com.rasmoo.cliente.escola.grade_curricular.models.entitys.Materia;
 import com.rasmoo.cliente.escola.grade_curricular.services.MateriaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -52,7 +48,10 @@ public class MateriaController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseDTO<MessageResponseDTO> createMateria(@Valid @RequestBody MateriaDTO materiaDTO) {
+    public ResponseDTO<MessageResponseDTO> createMateria(@Valid @RequestBody MateriaDTO materiaDTO) throws SendIdException {
+
+        if(materiaDTO.getId() != null)
+            throw new SendIdException("materia");
 
         ResponseDTO responseDTO = new ResponseDTO();
         responseDTO.setData(materiaService.createMateria(materiaDTO));
--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/services/MateriaService.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/services/MateriaService.java
@@ -8,8 +8,6 @@ import com.rasmoo.cliente.escola.grade_curricular.models.entitys.Materia;
 import com.rasmoo.cliente.escola.grade_curricular.repositories.MateriaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
