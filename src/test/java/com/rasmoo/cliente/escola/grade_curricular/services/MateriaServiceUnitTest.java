ca5072cc39c1ef967277bc9d7efaf58b1c76c160
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-10-15 21:11:00 -0300

    adiciona testes unitários na service para os métodos de buscar matérias por id

--- src/test/java/com/rasmoo/cliente/escola/grade_curricular/services/MateriaServiceUnitTest.java
+++ src/test/java/com/rasmoo/cliente/escola/grade_curricular/services/MateriaServiceUnitTest.java
@@ -1,6 +1,7 @@
 package com.rasmoo.cliente.escola.grade_curricular.services;
 
 import com.rasmoo.cliente.escola.grade_curricular.builders.MateriaDTOBuilder;
+import com.rasmoo.cliente.escola.grade_curricular.exceptions.MateriaNotFoundException;
 import com.rasmoo.cliente.escola.grade_curricular.mappers.MateriaMapper;
 import com.rasmoo.cliente.escola.grade_curricular.models.dto.MateriaDTO;
 import com.rasmoo.cliente.escola.grade_curricular.models.entitys.Materia;
@@ -11,17 +12,23 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class MateriaServiceUnitTest {
+
+    private static final Long VALID_MATERIA_ID = 1L;
+    private static final Long INVALID_MATERIA_ID = 2L;
 
     @Mock
     private MateriaRepository materiaRepository;
@@ -52,7 +59,6 @@ public class MateriaServiceUnitTest {
     @Test
     public void quandoListarTodasMateriasEhChamadoUmaListaVaziaDeveSerRetornada() {
 
-
         //when
         when(materiaRepository.findAll()).thenReturn(Collections.emptyList());
 
@@ -60,9 +66,80 @@ public class MateriaServiceUnitTest {
         List<MateriaDTO> materiaDTORetornada = materiaService.listMaterias();
 
         assertThat(materiaDTORetornada, is(empty()));
+        
+    }
 
+    @Test
+    public void quandoGetMateriaEhChamadoComIdValidoDeveSerRetornadoUmaMateria() throws MateriaNotFoundException {
+
+        //given
+        MateriaDTO expectedFoundMateriaDTO = MateriaDTOBuilder.builder().build().toMateriaDTO();
+        Materia expectedFoundMateria = materiaMapper.toModel(expectedFoundMateriaDTO);
+
+        //when
+        when(materiaRepository.findById(VALID_MATERIA_ID)).thenReturn(Optional.of(expectedFoundMateria));
+
+        //then
+        MateriaDTO materiaDTORetornada = materiaService.getMateriaById(VALID_MATERIA_ID);
+
+        assertThat(materiaDTORetornada, is(equalTo(expectedFoundMateriaDTO)));
 
     }
+
+    @Test
+    public void quandoGetMateriaEhChamadoComIdInvalidoDeveSerRetornadoExcecao() throws MateriaNotFoundException {
+
+        //when
+        when(materiaRepository.findById(INVALID_MATERIA_ID)).thenReturn(Optional.empty());
+
+        //then
+        assertThrows(MateriaNotFoundException.class, () -> materiaService.getMateriaById(INVALID_MATERIA_ID));
+
+    }
+
+    @Test
+    public void quandoFindMateriasEhChamadoComIdsValidosDeveSerRetornadoUmaListaDeMaterias() throws MateriaNotFoundException {
+
+        //given
+        List<Long> listaDeIds = new ArrayList<>();
+        listaDeIds.add(1L);
+        listaDeIds.add(2L);
+
+        MateriaDTO expectedFoundMateriaDTO1 = MateriaDTOBuilder.builder().build().toMateriaDTO();
+        MateriaDTO expectedFoundMateriaDTO2 = MateriaDTOBuilder.builder().build().toMateriaDTO();
+        expectedFoundMateriaDTO2.setId(2L);
+
+        Materia expectedFoundMateria1 = materiaMapper.toModel(expectedFoundMateriaDTO1);
+        Materia expectedFoundMateria2 = materiaMapper.toModel(expectedFoundMateriaDTO2);
+
+        //when
+        when(materiaRepository.findById(1L)).thenReturn(Optional.of(expectedFoundMateria1));
+        when(materiaRepository.findById(2L)).thenReturn(Optional.of(expectedFoundMateria2));
+
+        //then
+        List<Materia> materiasRetornada = materiaService.findMateriasByIds(listaDeIds);
+
+        assertThat(materiasRetornada, is(not(empty())));
+        assertThat(materiasRetornada.get(0), is(equalTo(expectedFoundMateria1)));
+        assertThat(materiasRetornada.get(1), is(equalTo(expectedFoundMateria2)));
+
+    }
+
+    @Test
+    public void quandoFindMateriasEhChamadoComIdsInvalidosDeveSerRetornadoUmaExcecao() throws MateriaNotFoundException {
+
+        //given
+        List<Long> listaDeIds = new ArrayList<>();
+        listaDeIds.add(INVALID_MATERIA_ID);
+
+        //when
+        when(materiaRepository.findById(INVALID_MATERIA_ID)).thenReturn(Optional.empty());
+
+        //then
+        assertThrows(MateriaNotFoundException.class, () -> materiaService.findMateriasByIds(listaDeIds));
+
+    }
+
 
 
 }
