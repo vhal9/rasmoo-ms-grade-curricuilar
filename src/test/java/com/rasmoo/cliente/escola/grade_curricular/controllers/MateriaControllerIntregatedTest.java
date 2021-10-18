f19c81c780a98a75bf2f01026cdff0d1b6274a77
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-10-17 23:47:17 -0300

    adiciona testes integrados para a rota de listar todas as matérias

---
+++ src/test/java/com/rasmoo/cliente/escola/grade_curricular/controllers/MateriaControllerIntregatedTest.java
@@ -0,0 +1,101 @@
+package com.rasmoo.cliente.escola.grade_curricular.controllers;
+
+import com.rasmoo.cliente.escola.grade_curricular.models.dto.MateriaDTO;
+import com.rasmoo.cliente.escola.grade_curricular.models.dto.ResponseDTO;
+import com.rasmoo.cliente.escola.grade_curricular.models.entitys.Materia;
+import com.rasmoo.cliente.escola.grade_curricular.repositories.MateriaRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(JUnitPlatform.class)
+public class MateriaControllerIntregatedTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private MateriaRepository materiaRepository;
+
+    @BeforeEach
+    void init() {
+        this.populaBaseDeDados();
+    }
+
+    @AfterEach
+    void finish() {
+        this.materiaRepository.deleteAll();
+    }
+
+    private void populaBaseDeDados() {
+
+        Materia materia1 = new Materia();
+        materia1.setNome("Prog. Orientada a Objetos");
+        materia1.setCodigo("POO");
+        materia1.setHoras(2);
+
+        Materia materia2 = new Materia();
+        materia2.setNome("Inteligencia Artificial");
+        materia2.setCodigo("IAL");
+        materia2.setHoras(4);
+
+        Materia materia3 = new Materia();
+        materia3.setNome("Computaçao Grafica");
+        materia3.setCodigo("COG");
+        materia3.setHoras(4);
+
+        this.materiaRepository.saveAll(Arrays.asList(materia1, materia2, materia3));
+
+    }
+
+    @Test
+    public void quandoGETListarTodosEhChamadoERetornaListaDeMaterias() throws Exception {
+
+        ResponseEntity<ResponseDTO<List<MateriaDTO>>> materias = restTemplate
+                .exchange("http://localhost:" + this.port + "/api/materias/",
+                        HttpMethod.GET, null,
+                        new ParameterizedTypeReference<ResponseDTO<List<MateriaDTO>>>() {
+                        });
+        assertNotNull(materias.getBody().getData());
+        assertEquals(200, materias.getBody().getHttpStatus());
+        assertEquals(3, materias.getBody().getData().size());
+
+    }
+
+    @Test
+    public void quandoGETListarTodosEhChamadoERetornaListaDeMateriasVazia() throws Exception {
+
+        this.finish();
+
+        ResponseEntity<ResponseDTO<List<MateriaDTO>>> materias = restTemplate
+                .exchange("http://localhost:" + this.port + "/api/materias/",
+                        HttpMethod.GET, null,
+                        new ParameterizedTypeReference<ResponseDTO<List<MateriaDTO>>>() {
+                        });
+        assertNotNull(materias.getBody().getData());
+        assertEquals(200, materias.getBody().getHttpStatus());
+        assertEquals(0, materias.getBody().getData().size());
+
+    }
+
+}
