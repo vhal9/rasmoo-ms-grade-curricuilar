4db049e394152f5fd4971c52ebceb083e781312d
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-12-30 12:00:09 -0300

    refatoraçao da controller advice

--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/controllers/ApplicationControllerAdvice.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/controllers/ApplicationControllerAdvice.java
@@ -1,10 +1,13 @@
 package com.rasmoo.cliente.escola.grade_curricular.controllers;
 
 import com.rasmoo.cliente.escola.grade_curricular.exceptions.CursoNotFoundException;
+import com.rasmoo.cliente.escola.grade_curricular.exceptions.EmailExistenteException;
 import com.rasmoo.cliente.escola.grade_curricular.exceptions.MateriaNotFoundException;
 import com.rasmoo.cliente.escola.grade_curricular.exceptions.SendIdException;
 import com.rasmoo.cliente.escola.grade_curricular.models.dto.ResponseDTO;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,11 +23,7 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseDTO<String> handleMateriaNotFoundException(MateriaNotFoundException ex) {
 
-        ResponseDTO responseDTO = new ResponseDTO();
-        responseDTO.setData(ex.getMessage());
-        responseDTO.setHttpStatus(HttpStatus.BAD_REQUEST.value());
-
-        return responseDTO;
+        return createResponseErroWithMessage(ex.getMessage());
 
     }
 
@@ -34,10 +33,10 @@ public class ApplicationControllerAdvice {
 
         List<String> erros = ex.getBindingResult().getAllErrors()
                 .stream()
-                .map( erro -> erro.getDefaultMessage())
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.toList());
 
-        ResponseDTO responseDTO = new ResponseDTO();
+        ResponseDTO<List<String>> responseDTO = new ResponseDTO();
         responseDTO.setData(erros);
         responseDTO.setHttpStatus(HttpStatus.BAD_REQUEST.value());
 
@@ -49,11 +48,7 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseDTO<String> handleSendIdException(SendIdException ex) {
 
-        ResponseDTO responseDTO = new ResponseDTO();
-        responseDTO.setData(ex.getMessage());
-        responseDTO.setHttpStatus(HttpStatus.BAD_REQUEST.value());
-
-        return responseDTO;
+        return createResponseErroWithMessage(ex.getMessage());
 
     }
 
@@ -61,8 +56,30 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseDTO<String> handleCursoNotFoundException(CursoNotFoundException ex) {
 
-        ResponseDTO responseDTO = new ResponseDTO();
-        responseDTO.setData(ex.getMessage());
+        return createResponseErroWithMessage(ex.getMessage());
+
+    }
+
+    @ExceptionHandler(EmailExistenteException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseDTO<String> handleEmailExistenteException(EmailExistenteException ex) {
+
+        return createResponseErroWithMessage(ex.getMessage());
+
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseDTO<String> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+
+        return createResponseErroWithMessage(ex.getMessage());
+
+    }
+
+    private ResponseDTO<String> createResponseErroWithMessage(String message) {
+
+        ResponseDTO<String> responseDTO = new ResponseDTO();
+        responseDTO.setData(message);
         responseDTO.setHttpStatus(HttpStatus.BAD_REQUEST.value());
 
         return responseDTO;
