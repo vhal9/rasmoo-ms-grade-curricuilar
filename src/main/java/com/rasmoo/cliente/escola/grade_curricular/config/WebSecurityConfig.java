db45fe7eaa8132cb6eb0ce5b8c7fe15aa8e246c6
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-12-30 11:51:29 -0300

    Adiciona login com Spring Security e usuario no banco

--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/config/WebSecurityConfig.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/config/WebSecurityConfig.java
@@ -1,30 +1,42 @@
 package com.rasmoo.cliente.escola.grade_curricular.config;
 
+import com.rasmoo.cliente.escola.grade_curricular.services.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    public void configuracaoGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-        PasswordEncoder pass = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication().withUser("rasmoo").
-                password(pass.encode("msgradecurricular")).roles("ADMIN");
+    @Autowired
+    public void configuracaoGlobal(AuthenticationManagerBuilder auth, UserInfoService userInfo) throws Exception {
+
+        auth.userDetailsService(userInfo).passwordEncoder(this.encoder());
 
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        String[] allowed = new String[] {
+                "/webjars", "/api/usuarios", "static/**"
+        };
+
         http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers(allowed).permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
