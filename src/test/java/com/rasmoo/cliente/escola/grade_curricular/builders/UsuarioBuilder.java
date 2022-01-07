95c4eabe1d5e1e55d2c2d273d5848e679be4e50d
Victor Hugo Landin <victor.h.landin@gmail.com>
2022-01-07 20:05:31 -0300

    refatora testes integrados da classe materia para funcionar apos adiçao do login

---
+++ src/test/java/com/rasmoo/cliente/escola/grade_curricular/builders/UsuarioBuilder.java
@@ -0,0 +1,26 @@
+package com.rasmoo.cliente.escola.grade_curricular.builders;
+
+import com.rasmoo.cliente.escola.grade_curricular.models.entitys.Credencial;
+import com.rasmoo.cliente.escola.grade_curricular.models.entitys.Usuario;
+import lombok.Builder;
+
+@Builder
+public class UsuarioBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String nome = "teste";
+
+    @Builder.Default
+    private Credencial credencial = new Credencial();
+
+    public Usuario toUsuario() {
+        credencial.setEmail("teste@teste.com");
+        credencial.setSenha("1111");
+        return new Usuario(id,
+                nome,
+                credencial);
+    }
+}
