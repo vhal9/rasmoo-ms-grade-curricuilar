0ac95c114fa289b10ab4d713ab866773d40ccee1
Victor Hugo Landin <victor.h.landin@gmail.com>
2022-01-07 19:26:50 -0300

    adiciona configuraçao STATELESS na autenticaçao

--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/config/WebSecurityConfig.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/config/WebSecurityConfig.java
@@ -3,13 +3,12 @@ package com.rasmoo.cliente.escola.grade_curricular.config;
 import com.rasmoo.cliente.escola.grade_curricular.services.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -30,11 +29,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        String[] allowed = new String[] {
+        String[] allowed = new String[]{
                 "/webjars", "/api/usuarios", "static/**"
         };
 
         http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers(allowed).permitAll()
                 .anyRequest()
