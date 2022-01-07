8a376aa0be7a695a644dd404be24ec8b62e28c03
Victor Hugo Landin <victor.h.landin@gmail.com>
2022-01-07 20:03:57 -0300

    adiciona construtores na classe usuario

--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/models/entitys/Usuario.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/models/entitys/Usuario.java
@@ -1,12 +1,16 @@
 package com.rasmoo.cliente.escola.grade_curricular.models.entitys;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_user")
 public class Usuario {
