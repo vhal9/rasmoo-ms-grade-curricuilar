4e604fef7a98fdac7bc1057491fdca9afd475baf
Victor Hugo Landin <victor.h.landin@gmail.com>
2022-03-23 14:29:46 -0300

    Adiciona implementaçao do JWT para assinatura dos tokens de acesso

--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/auth/AuthorizationServer.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/auth/AuthorizationServer.java
@@ -4,17 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
 import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.sql.DataSource;
 
@@ -32,30 +32,44 @@ public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
     @Qualifier("dsOAuth")
     private DataSource dataSource;
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
-    }
-
-    @Bean
-    public ApprovalStore approvalStore() {
-        return new JdbcApprovalStore(dataSource);
-    }
-
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
 
         DefaultOAuth2RequestFactory requestFactory = new DefaultOAuth2RequestFactory(clientDetailsService);
         requestFactory.setCheckUserScopes(Boolean.TRUE);
 
-        endpoints.authenticationManager(authenticationManager)
+        endpoints
+                .authenticationManager(authenticationManager)
                 .requestFactory(requestFactory)
-                .approvalStore(this.approvalStore())
-                .tokenStore(this.tokenStore());
+                .accessTokenConverter(this.accessTokenConverter())
+                .tokenStore(this.jwtTokenStore());
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.jdbc(this.dataSource);
     }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey("assinatura-gradeControll");
+
+        return accessTokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore jwtTokenStore() {
+        return new JwtTokenStore(this.accessTokenConverter());
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(this.jwtTokenStore());
+        tokenServices.setSupportRefreshToken(Boolean.TRUE);
+        return tokenServices;
+    }
+
 }
--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/auth/ResourceServer.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/auth/ResourceServer.java
@@ -1,18 +1,31 @@
 package com.rasmoo.cliente.escola.grade_curricular.auth;
 
+import com.rasmoo.cliente.escola.grade_curricular.services.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.*;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@Configuration
 @EnableResourceServer
 public class ResourceServer extends ResourceServerConfigurerAdapter {
 
     public static final String RESOURCE_ID = "gradeControll";
 
+    @Autowired
+    private UserInfoService detalhesDoUsuario;
+
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
         resources.resourceId(RESOURCE_ID);
+        resources.tokenServices(this.defaultTokenServices());
+        resources.tokenStore(this.tokenStore());
     }
 
     @Override
@@ -21,4 +34,39 @@ public class ResourceServer extends ResourceServerConfigurerAdapter {
                 .cors();
 
     }
+
+    @Bean
+    public UserAuthenticationConverter userTokenConverter() {
+        DefaultUserAuthenticationConverter converter = new DefaultUserAuthenticationConverter();
+        converter.setUserDetailsService(detalhesDoUsuario);
+        return converter;
+    }
+
+    @Bean
+    public AccessTokenConverter defaultAccessTokenConverter() {
+        DefaultAccessTokenConverter tokenConverter = new DefaultAccessTokenConverter();
+        tokenConverter.setUserTokenConverter(this.userTokenConverter());
+        return tokenConverter;
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setAccessTokenConverter(this.defaultAccessTokenConverter());
+        accessTokenConverter.setSigningKey("assinatura-gradeControll");
+        return accessTokenConverter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(this.jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public DefaultTokenServices defaultTokenServices() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(this.tokenStore());
+        return tokenServices;
+    }
+
 }
