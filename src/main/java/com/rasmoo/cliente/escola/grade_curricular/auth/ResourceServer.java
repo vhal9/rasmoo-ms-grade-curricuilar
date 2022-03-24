0f89f0c64aa74a887b481bc5edcd8fb00b1c5193
Victor Hugo Landin <victor.h.landin@gmail.com>
2022-03-24 17:38:09 -0300

    Refatoraçao do Swagger para utilizaçao do jwt e oauth2

--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/auth/ResourceServer.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/auth/ResourceServer.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 import org.springframework.security.oauth2.provider.token.*;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -30,8 +32,13 @@ public class ResourceServer extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and()
-                .cors();
+        http.authorizeRequests()
+                .antMatchers("/v2/api-docs", "/swagger*/**", "/webjars/")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler())
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().cors();
 
     }
 
