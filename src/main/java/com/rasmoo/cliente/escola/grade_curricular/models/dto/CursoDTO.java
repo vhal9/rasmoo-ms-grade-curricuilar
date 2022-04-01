23cf62254cb6b9319b5e89e8213163b4d0a6643e
Victor Hugo Landin <victor.h.landin@gmail.com>
2022-03-31 21:16:59 -0300

    adiciona centralização de mensagens para os fluxos da entidade curso

--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/models/dto/CursoDTO.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/models/dto/CursoDTO.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -17,12 +18,12 @@ public class CursoDTO {
 
     private Long id;
 
-    @NotEmpty(message = "Campo nome é obrigatório")
-    @Size(min = 3, max = 30, message = "Campo nome deve ter entre 3 e 30 caracteres")
+    @NotEmpty(message = "{curso.campo-nome.obrigatorio}")
+    @Size(min = 3, max = 30, message = "{curso.campo-nome.tamanho}")
     private String nome;
 
-    @NotEmpty(message = "Campo código é obrigatório")
-    @Size(min = 3, max = 5, message = "Campo código deve ter entre 3 e 5 caracteres")
+    @NotEmpty(message = "{curso.campo-codigo.obrigatorio}")
+    @Size(min = 3, max = 10, message = "{curso.campo-codigo.tamanho}")
     private String codigo;
 
     private List<Long> idsMaterias = new ArrayList<>();
