5424102af502a4e2f45496f8b39dca2727aaaa43
Victor Hugo Landin <victor.h.landin@gmail.com>
2022-04-23 21:21:17 -0300

    diminui acoplamento da service de Usuario

--- src/main/java/com/rasmoo/cliente/escola/grade_curricular/services/UserInfoService.java
+++ src/main/java/com/rasmoo/cliente/escola/grade_curricular/services/UserInfoService.java
@@ -2,8 +2,7 @@ package com.rasmoo.cliente.escola.grade_curricular.services;
 
 import com.rasmoo.cliente.escola.grade_curricular.core.impl.ResourceOwner;
 import com.rasmoo.cliente.escola.grade_curricular.models.entitys.Usuario;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,10 +11,10 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor
 public class UserInfoService implements UserDetailsService {
 
-    private UsuarioService usuarioService;
+    private final UsuarioService usuarioService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
